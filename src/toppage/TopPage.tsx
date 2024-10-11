0c2394d14817b6c5e712f118d44cbfaef59a9c30
louis78780610 <rui.m.ycrk.lucky7@gmail.com>
2024-10-11 17:55:20 +0900

    task2完了

--- src/toppage/TopPage.tsx
+++ src/toppage/TopPage.tsx
@@ -8,6 +8,7 @@ const TopPage = () => {
       <h1 className='text-6xl font-serif italic mb-4 text-neutral-700 text-center sm:text-4xl'>Wellcome</h1>
       <div className='flex justify-center p-12 sm:p-4 md:flex md:flex-col md:items-center sm:items-center'>
         <Link to="task1"><Button onClick={() => {}} children="Task1"/></Link>
+        <Link to="task2"><Button onClick={() => {}} children="Task2"/></Link>
       </div>
       </div>
     </>
