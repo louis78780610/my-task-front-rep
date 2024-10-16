839f3d2c3bbd699f2abdab701b1dfee430a2d8ac
louis78780610 <rui.m.ycrk.lucky7@gmail.com>
2024-10-16 18:25:27 +0900

    task5完了

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Task1 from '../src/task1/Task1';
 import Task2 from '../src/task2/Task2';
 import Task3 from './task3/Task3';
 import Task4 from './task4/Task4';
+import Task5 from './task5/Task5';
 
 
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/Task2" element={<Task2 />} />
         <Route path="/Task3" element={<Task3 />} />
         <Route path="/Task4" element={<Task4 />} />
+        <Route path="/Task5" element={<Task5 />} />
       </Routes>
     </Router>
   );
